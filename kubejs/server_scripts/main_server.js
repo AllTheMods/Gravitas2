55e32970976da62e1f505bd11493c71792da2784
pietro-lopes <unknownrj@gmail.com>
2023-09-20 22:30:54 -0300

    Modified recipes, block and chest loot tables
    Banned copper ingot from entities
    Replaced vanilla torches to tfc torches on some recipes
    Added Jade tooltips to some blocks

--- kubejs/server_scripts/main_server.js
+++ kubejs/server_scripts/main_server.js
@@ -2,11 +2,15 @@
 
 ServerEvents.recipes((event) => {
   recipeRemoval(event)
+  replaceRecipes(event)
   recipeAdd(event)
 })
 
 LootJS.modifiers((event) => {
+  //event.enableLogging()
   modifyAndRemoveChestLoot(event)
+  modifyAndRemoveBlockLoot(event)
+  modifyAndRemoveEntityLoot(event)
 })
 
 NetworkEvents.dataReceived("customTask", (event) => {
