38d0531b16ed611bbd64fe20af91fb8fded897a4
Drackion <christian.seach@gmail.com>
2023-09-29 14:01:24 +1000

    invar recipes

--- kubejs/startup_scripts/registry/items.js
+++ kubejs/startup_scripts/registry/items.js
@@ -3,3 +3,7 @@
 let registerItems = (/** @type {Registry.Item} */ event) => {
   event.create("kubejs:cutter_head").unstackable().displayName("Cutter Head")
 }
+
+let doubleIngots = (/** @type {Registry.Item} */ event) => {
+  event.create('gtceu:double_invar_ingot').displayName('Invar Double Ingot').texture('tfc:item/metal/double_ingot/wrought_iron').color(0, 0xB4B478)
+}
