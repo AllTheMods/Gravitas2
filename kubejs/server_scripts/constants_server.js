e72f58d81dafd8c5f79fe9139a87697dc0547ffe
pietro-lopes <unknownrj@gmail.com>
2023-10-07 17:36:31 -0300

    moving import java classes to a single main file

---
+++ kubejs/server_scripts/constants_server.js
@@ -0,0 +1,5 @@
+// priority 100
+
+const $IngotPileBlock = Java.loadClass("net.dries007.tfc.common.blocks.devices.IngotPileBlock")
+const $Integer = Java.loadClass('java.lang.Integer');
+const $Registries = Java.loadClass("net.minecraft.core.registries.Registries")
