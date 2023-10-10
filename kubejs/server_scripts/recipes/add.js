42726cebad7a56fbac35033ffad585a2f8611800
Len Kagamine <xboxmaster2000@hotmail.com>
2023-10-09 23:45:45 -0400

    Initial Fix?
    Hopefully.

--- kubejs/server_scripts/recipes/add.js
+++ kubejs/server_scripts/recipes/add.js
@@ -3,7 +3,7 @@
 let recipeAdd = (/** @type {Internal.RecipesEventJS} */ event) => {
   event.custom({
     type: "tfc:anvil",
-    input: {tag: "forge:ingots/wrought_iron"},
+    input: { tag: "forge:ingots/wrought_iron" },
     result: {
       item: "gtceu:wrought_iron_rod",
       count: 2,
@@ -14,18 +14,18 @@ let recipeAdd = (/** @type {Internal.RecipesEventJS} */ event) => {
 
   event.custom({
     type: "tfc:welding",
-    first_input: {item: "gtceu:wrought_iron_rod"},
-    second_input: {item: "gtceu:wrought_iron_rod"},
+    first_input: { item: "gtceu:wrought_iron_rod" },
+    second_input: { item: "gtceu:wrought_iron_rod" },
     tier: 3,
-    result: {item: "kubejs:cutter_head"},
+    result: { item: "kubejs:cutter_head" },
   })
 
   event.custom({
     type: "tfc:welding",
-    first_input: {item: "gtceu:wrought_iron_bolt"},
-    second_input: {item: "gtceu:wrought_iron_bolt"},
+    first_input: { item: "gtceu:wrought_iron_bolt" },
+    second_input: { item: "gtceu:wrought_iron_bolt" },
     tier: 3,
-    result: {item: "gtceu:wrought_iron_screw"},
+    result: { item: "gtceu:wrought_iron_screw" },
   })
 
   event.shaped("gtceu:wrought_iron_wire_cutter", [" F ", "HCD", "RSR"], {
@@ -47,20 +47,59 @@ let recipeAdd = (/** @type {Internal.RecipesEventJS} */ event) => {
       item: "gtceu:glass_tube",
     },
   })
-  event.shaped("create:andesite_alloy",["SZ ", "ZS ", "   "],{
-   S: "#tfc:igneous_extrusive_rock",
-   Z: "#forge:nuggets/zinc"
+
+  //Create Start
+  event.shaped("create:andesite_alloy", ["SZ ", "ZS ", "   "], {
+    S: "#tfc:igneous_extrusive_rock",
+    Z: "#forge:nuggets/zinc",
   })
 
-  event.shaped("framedblocks:framed_chest",["FRF","RCR","FRF"],{
-   F: "framedblocks:framed_cube",
-   R: "#forge:rods/cast_iron",
-   C: "#forge:chests/wooden"
+  event.custom({
+    type: "create:mixing",
+    ingredients: [{ tag: "tfc:igneous_extrusive_rock" }, { tag: "forge:nuggets/zinc" }],
+    results: [{ item: "create:andesite_alloy" }],
   })
 
-  event.shaped("framedblocks:framed_secret_storage",["RFR","FCF","RFR"],{
-     F: "framedblocks:framed_cube",
-     R: "#forge:rods/cast_iron",
-     C: "framedblocks:framed_chest"
-    })
+  event.custom({
+    type: "create:pressing",
+    ingredients: [{ item: "tfc:raw_iron_bloom" }],
+    results: [{ item: "tfc:refined_iron_bloom" }],
+  })
+  event.custom({
+    type: "create:pressing",
+    ingredients: [{ item: "tfc:refined_iron_bloom" }],
+    results: [{ item: "tfc:metal/ingot/wrought_iron" }],
+  })
+  //Create End
+
+  //GTCEU Start
+  event.shaped("gtceu:primitive_blast_furnace", ["HRS", "PBR", "DRS"], {
+    H: "#forge:tools/hammers",
+    R: "#forge:rods/steel",
+    S: "#forge:screws/steel",
+    P: "#forge:sheets/steel",
+    B: "gtceu:firebricks",
+    D: "#forge:tools/screwdrivers",
+  })
+  //GTCEU End
+
+  //Railcraft Start
+  event.shaped("railcraft:solid_fueled_firebox", ["BBB", "BCB", "BFB"], {
+    B: "minecraft:brick",
+    C: "minecraft:fire_charge",
+    F: "tfc:crucible",
+  })
+
+  event.shaped("framedblocks:framed_chest", ["FRF", "RCR", "FRF"], {
+    F: "framedblocks:framed_cube",
+    R: "#forge:rods/cast_iron",
+    C: "#forge:chests/wooden",
+  })
+
+  event.shaped("framedblocks:framed_secret_storage", ["RFR", "FCF", "RFR"], {
+    F: "framedblocks:framed_cube",
+    R: "#forge:rods/cast_iron",
+    C: "framedblocks:framed_chest",
+  })
+  //Railcraft End
 }
--- kubejs/server_scripts/recipes/replace.js
+++ kubejs/server_scripts/recipes/replace.js
@@ -1,6 +1,42 @@
 // priority 10
 
 const itemTypes = ["ingots", "plates", "rods", "gears"]
+
+//Id's of things to replace furnace -> crucible
+const furnacent = [
+  "integrateddynamics:crafting/part_machine_reader",
+  "gtceu:shaped/multi_furnace",
+  "integrateddynamics:crafting/part_machine_writer",
+  "gtceu:shaped/steam_boiler_coal_bronze",
+  "gtceu:shaped/steam_boiler_coal_steel",
+  "railcraft:fluid_fueled_firebox",
+  "enderio:alloy_smelter",
+  "minecraft:blast_furnace",
+  "gtceu:shaped/steam_furnace_bronze",
+  "gtceu:shaped/steam_alloy_smelter_bronze",
+  "gtceu:shaped/electric_blast_furnace",
+  "enderio:stirling_generator",
+  "railcraft:steam_oven",
+  "ad_astra:recipes/fuel_refinery",
+  "enderio:primitive_alloy_smelter",
+  "ae2:network/blocks/energy_vibration_chamber",
+  "railcraft:tunnel_bore",
+  "ad_astra:recipes/coal_generator",
+  "integrateddynamics:crafting/coal_generator",
+]
+
 let replaceRecipes = (/** @type {Internal.RecipesEventJS} */ event) => {
   event.replaceOutput({ type: "minecraft:crafting_shaped" }, "minecraft:torch", "tfc:torch")
+  //remove furnace existance
+  furnacent.forEach((furnace) => {
+    event.replaceInput(furnace, "minecraft:furnace", "tfc:crucible")
+  })
+
+  //Create
+  event.replaceInput({ type: "minecraft:crafting_shaped" }, "minecraft:dried_kelp", "tfc:food/dried_kelp")
+  event.replaceOutput({ id: "minecraft:dried_kelp" }, "minecraft:dried_kelp", "tfc:food/dried_kelp")
+
+  //Railcraft
+  event.replaceInput({ id: "railcraft:water_tank_siding" }, "minecraft:slimeball", "gtceu:sticky_resin")
+  event.replaceInput({ id: "railcraft:steam_locomotive" }, "railcraft:blast_furnace_bricks", "gtceu:firebricks")
 }
