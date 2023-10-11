d9c0ac3f2ba633c38870296f0dbe8fe283d6c9a5
pietro-lopes <unknownrj@gmail.com>
2023-10-11 13:55:00 -0300

    formating

--- kubejs/server_scripts/datapack/greg_ingots.js
+++ kubejs/server_scripts/datapack/greg_ingots.js
@@ -1,18 +1,19 @@
 // priority 10
+// @ts-check
 
-let addGregTechIngotsToTFC = (/** @type {Internal.DataPackEventJS} */ event) => {
+const addGregTechIngotsToTFC = (/** @type {Internal.DataPackEventJS} */ event) => {
   /**
    * @param {string} item
    * @param {number} heat - Heat Capacity
    * @param {number} [forgTemp] - Optional: Forging Temperature
    * @param {number} [weldTemp] - Optional: Welding Temperature
    */
-  let addTFCHeatCapability = (item, heat, forgTemp, weldTemp) => {
+  const addTFCHeatCapability = (item, heat, forgTemp, weldTemp) => {
     /** @type {Internal.LinkedHashMap} */
-    let json = JsonIO.toObject({ ingredient: Item.of(item).toJson(), heat_capacity: heat })
-    forgTemp && json.put("forging_temperature", forgTemp)
-    weldTemp && json.put("welding_temperature", weldTemp)
-    let itemLocation = Utils.id(item)
+    const json = JsonIO.toObject({ ingredient: Item.of(item).toJson(), heat_capacity: heat })
+    forgTemp && json.put('forging_temperature', forgTemp)
+    weldTemp && json.put('welding_temperature', weldTemp)
+    const itemLocation = Utils.id(item)
     event.addJson(`${itemLocation.namespace}:tfc/item_heats/metal/${itemLocation.path}.json`, json)
   }
 
@@ -21,10 +22,10 @@ let addGregTechIngotsToTFC = (/** @type {Internal.DataPackEventJS} */ event) => 
    * @param {"tiny"|"very_small"|"small"|"normal"|"large"|"very_large"|"huge"} size
    * @param {"very_light"|"light"|"medium"|"heavy"|"very_heavy"} weight
    */
-  let addTFCSize = (item, size, weight) => {
+  const addTFCSize = (item, size, weight) => {
     /** @type {Internal.LinkedHashMap} */
-    let json = JsonIO.toObject({ ingredient: Item.of(item).toJson(), size: size, weight: weight })
-    let itemLocation = Utils.id(item)
+    const json = JsonIO.toObject({ ingredient: Item.of(item).toJson(), size: size, weight: weight })
+    const itemLocation = Utils.id(item)
     event.addJson(`${itemLocation.namespace}:tfc/item_sizes/${itemLocation.path}.json`, json)
   }
 
@@ -37,69 +38,69 @@ let addGregTechIngotsToTFC = (/** @type {Internal.DataPackEventJS} */ event) => 
    * @param {string} ingots
    * @param {string} sheets
    */
-  let addTFCMetalFluid = (fluid, tier, meltTemp, specificHeatCap, ingots, sheets) => {
+  const addTFCMetalFluid = (fluid, tier, meltTemp, specificHeatCap, ingots, sheets) => {
     /** @type {Internal.LinkedHashMap} */
-    let json = JsonIO.toObject({
+    const json = JsonIO.toObject({
       tier: tier,
       fluid: fluid,
       melt_temperature: meltTemp,
       specific_heat_capacity: specificHeatCap,
       ingots: Ingredient.of(ingots).toJson(),
-      sheets: Ingredient.of(sheets).toJson(),
+      sheets: Ingredient.of(sheets).toJson()
     })
-    let itemLocation = Utils.id(fluid)
+    const itemLocation = Utils.id(fluid)
     event.addJson(`${itemLocation.namespace}:tfc/metals/${itemLocation.path}.json`, json)
   }
 
-  let addTFCHeatingRecipe = (item, fluid, meltTemp, amount) => {
+  const addTFCHeatingRecipe = (item, fluid, meltTemp, amount) => {
     /** @type {Internal.LinkedHashMap} */
-    let json = JsonIO.toObject({
-      type: "tfc:heating",
+    const json = JsonIO.toObject({
+      type: 'tfc:heating',
       ingredient: Ingredient.of(item).toJson(),
       result_fluid: {
         fluid: fluid,
-        amount: amount,
+        amount: amount
       },
-      temperature: meltTemp,
+      temperature: meltTemp
     })
-    let itemLocation = Utils.id(item)
+    const itemLocation = Utils.id(item)
     event.addJson(`tfc:recipes/heating/metal/${itemLocation.path}.json`, json)
   }
 
-  let addTFCCastingRecipe = (item, fluid, amount) => {
+  const addTFCCastingRecipe = (item, fluid, amount) => {
     /** @type {Internal.LinkedHashMap} */
-    let json = JsonIO.toObject({
-      type: "tfc:casting",
+    const json = JsonIO.toObject({
+      type: 'tfc:casting',
       mold: {
-        item: "tfc:ceramic/ingot_mold",
+        item: 'tfc:ceramic/ingot_mold'
       },
       fluid: {
         ingredient: fluid,
-        amount: amount,
+        amount: amount
       },
       result: {
-        item: item,
+        item: item
       },
-      break_chance: 0.1,
+      break_chance: 0.1
     })
-    let itemLocation = Utils.id(item)
+    const itemLocation = Utils.id(item)
     event.addJson(`tfc:recipes/casting/${itemLocation.path}.json`, json)
   }
 
   gtceuIngots.forEach((id) => {
-    let fluid = Utils.id("gtceu:" + id)
-    let ingot = Utils.id("gtceu:" + id + "_ingot")
+    const fluid = Utils.id('gtceu:' + id)
+    const ingot = Utils.id('gtceu:' + id + '_ingot')
     let temp = $FluidHelper.getTemperature(Fluid.of(fluid)) - 273 // Kelvin to Celcius
     temp = Math.max(230, temp) // set at minimum temp like Tin
     addTFCHeatCapability(ingot.toString(), 2.857, Math.floor(temp * 0.6), Math.floor(temp * 0.8))
-    addTFCMetalFluid(fluid.toString(), 3, temp, 0.0085, "#forge:ingots/" + id, "#forge:plates/" + id)
+    addTFCMetalFluid(fluid.toString(), 3, temp, 0.0085, '#forge:ingots/' + id, '#forge:plates/' + id)
     if (temp > 1540) return // skip if temp is over Red/Blue Steel
     addTFCHeatingRecipe(ingot.toString(), fluid.toString(), temp, 144)
     addTFCCastingRecipe(ingot.toString(), fluid.toString(), 144)
   })
 
-  addTFCHeatCapability("gtceu:double_invar_ingot", 2.857, 921, 1228)
-  addTFCHeatCapability("gtceu:wrought_iron_bolt", 1.429, 921, 1228)
+  addTFCHeatCapability('gtceu:double_invar_ingot', 2.857, 921, 1228)
+  addTFCHeatCapability('gtceu:wrought_iron_bolt', 1.429, 921, 1228)
 
-  addTFCSize("gtceu:double_invar_ingot", "large", "heavy")
+  addTFCSize('gtceu:double_invar_ingot', 'large', 'heavy')
 }
--- kubejs/server_scripts/worldgen/gold_block_to_pile.js
+++ kubejs/server_scripts/worldgen/gold_block_to_pile.js
@@ -1,25 +1,28 @@
 // priority: 10
-const metals = ["tfc:metal/ingot/gold","tfc:metal/ingot/copper","tfc:metal/ingot/wrought_iron"]
+const pileMetals = ['tfc:metal/ingot/gold', 'tfc:metal/ingot/copper', 'tfc:metal/ingot/wrought_iron']
 
-let replaceGoldBlocksWithPiles = event => {
-  let level = event.worldGenLevel
-  event.intersectionBoxes.forEach(bb => {
-    BlockPos.betweenClosed(bb.minX(), bb.minY(), bb.minZ(), bb.maxX(), bb.maxY(), bb.maxZ()).forEach(pos => {
-      if (level.getBlockState(pos).block.id == "minecraft:gold_block"){
-        let metal = metals[Utils.random.nextInt(0,metals.length)]
-        let count = Utils.random.nextInt(1,65) // not inclusive to exclusive, so 65
-        let nbt = generateNbt(metal,count)
-        let pileBlockState = Block.getBlock("tfc:ingot_pile").defaultBlockState().setValue($IngotPileBlock.COUNT, $Integer.valueOf(Number(count).toFixed()))
+const replaceGoldBlocksWithPiles = (event) => {
+  const level = event.worldGenLevel
+  event.intersectionBoxes.forEach((bb) => {
+    let metal, count, nbt, pileBlockState, pile
+    BlockPos.betweenClosed(bb.minX(), bb.minY(), bb.minZ(), bb.maxX(), bb.maxY(), bb.maxZ()).forEach((pos) => {
+      if (level.getBlockState(pos).block.id === 'minecraft:gold_block') {
+        metal = pileMetals[Utils.random.nextInt(0, pileMetals.length)]
+        count = Utils.random.nextInt(1, 65) // inclusive to exclusive, so 65
+        nbt = generateNbt(metal, count)
+        pileBlockState = Block.getBlock('tfc:ingot_pile')
+          .defaultBlockState()
+          .setValue($IngotPileBlock.COUNT, $Integer.valueOf(Number(count).toFixed()))
         level.setBlock(pos, pileBlockState, 2)
-        let pile = level.getBlockEntity(pos)
+        pile = level.getBlockEntity(pos)
         pile.load(nbt)
       }
     })
   })
 }
 
-function generateNbt(metal, count){
-    let item = `{ForgeCaps: {"tfc:item_heat": {heat: 0,ticks: 0}},id: "${metal}",Count: 1}`
-    let array = Array(count).fill(item)
-    return `{stacks: [${array}],id: "tfc:ingot_pile"}`
+function generateNbt (metal, count) {
+  const item = `{ForgeCaps: {"tfc:item_heat": {heat: 0,ticks: 0}},id: "${metal}",Count: 1}`
+  const array = Array(count).fill(item)
+  return `{stacks: [${array}],id: "tfc:ingot_pile"}`
 }
