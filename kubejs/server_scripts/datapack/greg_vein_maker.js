22cbc51eed54f3ed27c1297f7d8f27847b9edda7
Len Kagamine <xboxmaster2000@hotmail.com>
2023-10-17 07:25:30 -0400

    More betterer
    Now with height option and more greg looking!

--- kubejs/server_scripts/datapack/greg_vein_maker.js
+++ kubejs/server_scripts/datapack/greg_vein_maker.js
@@ -1,16 +1,16 @@
 const addGregVeinData = (/** @type {Internal.DataPackEventJS} */ event) => {
   /**
    *
-   * @param {string} toReplace - Block to replace
    * @param {Array<Array<>>} blockToWeightMap - [["modid:block",weight],[etc...]]
    * @param {number} chancePerChunk - spawns 1/x chunks
-   * @param {number} minY - Lowest Y
-   * @param {number} maxY - Highest Y
+   * @param {number} minY - Lowest Y generation level
+   * @param {number} maxY - Highest Y generation level
    * @param {number} sizeIB - Radius
    * @param {number} densityIB - default 0.2
+   * @param {number} veinHeight - how tall the vein
    * @param {string} veinName - Name.
    */
-  const makeTFCDiskVein = (toReplace, blockToWeightMap, chancePerChunk, minY, maxY, sizeIB, densityIB, veinName) => {
+  const makeTFCDiskVein = (blockToWeightMap, chancePerChunk, minY, maxY, sizeIB, densityIB, veinHeight, veinName) => {
     let tempArray = []
     for (const subArray of blockToWeightMap) {
       tempArray.push(
@@ -20,8 +20,12 @@ const addGregVeinData = (/** @type {Internal.DataPackEventJS} */ event) => {
         })
       )
     }
+    let blockarray = []
+    tfcStone.forEach((stone) => {
+      blockarray.push(JsonIO.toObject({ replace: [`tfc:rock/raw/${stone}`], with: tempArray }))
+    })
     let json = JsonIO.toObject({
-      type: "tfc:cluster_vein",
+      type: "tfc:disc_vein",
       config: {
         rarity: chancePerChunk,
         min_y: {
@@ -32,15 +36,12 @@ const addGregVeinData = (/** @type {Internal.DataPackEventJS} */ event) => {
         },
         size: sizeIB,
         density: densityIB,
-        blocks: [
-          {
-            replace: [toReplace],
-            with: tempArray
-          }
-        ],
-        random_name: veinName
+        blocks: blockarray,
+        random_name: veinName,
+        height: veinHeight
       }
     })
+    console.log(json)
     event.addJson(`gregitas:worldgen/configured_feature/vein/${veinName}.json`, json)
     let json2 = JsonIO.toObject({
       feature: `gregitas:vein/${veinName}`,
@@ -50,19 +51,17 @@ const addGregVeinData = (/** @type {Internal.DataPackEventJS} */ event) => {
   }
   //Make sure to add anything you add here to `greg_vein_tags.js`
   //Below is an example of how to add a new vein.
-  tfcStone.forEach((stone) => {
-    makeTFCDiskVein(
-      `tfc:rock/raw/${stone}`,
-      [
-        ["gtceu:deepslate_diamond_ore", 1],
-        ["gtceu:deepslate_diamond_ore", 1]
-      ],
-      5,
-      -30,
-      30,
-      10,
-      0.2,
-      `test_${stone}`
-    )
-  })
+  makeTFCDiskVein(
+    [
+      ["gtceu:deepslate_diamond_ore", 1],
+      ["gtceu:deepslate_diamond_ore", 1]
+    ],
+    5,
+    -30,
+    30,
+    10,
+    0.2,
+    4,
+    `test_diamond`
+  )
 }
--- kubejs/server_scripts/tags/greg_vein_tags.js
+++ kubejs/server_scripts/tags/greg_vein_tags.js
@@ -1,7 +1,5 @@
 const addGregVeinsToTags = (/** @type {TagEvent.PlacedFeature}*/ event) => {
   //these are made in `greg_vein_maker.js`
   //Name should always be `gregitas:vein/${name of vein}`
-  tfcStone.forEach((stone) => {
-    event.add("tfc:in_biome/veins", `gregitas:vein/test_${stone}`)
-  })
+  event.add("tfc:in_biome/veins", `gregitas:vein/test_diamond`)
 }
