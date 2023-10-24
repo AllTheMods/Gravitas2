179cd1bc2f2a8ff9143e0e918b8fbbdcdc0582ae
screret <68943070+screret@users.noreply.github.com>
2023-10-24 19:13:27 +0300

    try stuff

--- kubejs/server_scripts/recipes/add.js
+++ kubejs/server_scripts/recipes/add.js
@@ -1,41 +1,51 @@
 // priority 10
 
 let recipeAdd = (/** @type {Internal.RecipesEventJS} */ event) => {
-  event.custom({
-    type: "tfc:anvil",
-    input: {tag: "forge:ingots/wrought_iron"},
-    result: {
-      item: "gtceu:wrought_iron_rod",
-      count: 2,
-    },
-    tier: 3,
-    rules: ["bend_last", "draw_second_last", "draw_third_last"],
-  })
-
-  event.custom({
-    type: "tfc:welding",
-    first_input: {item: "gtceu:wrought_iron_rod"},
-    second_input: {item: "gtceu:wrought_iron_rod"},
-    tier: 3,
-    result: {item: "kubejs:cutter_head"},
-  })
-
-  event.custom({
-    type: "tfc:welding",
-    first_input: {item: "gtceu:wrought_iron_bolt"},
-    second_input: {item: "gtceu:wrought_iron_bolt"},
-    tier: 3,
-    result: {item: "gtceu:wrought_iron_screw"},
-  })
-
-  event.shaped("gtceu:wrought_iron_wire_cutter", [" F ", "HCD", "RSR"], {
-    F: "#forge:tools/files",
-    H: "#forge:tools/hammers",
-    C: "kubejs:cutter_head",
-    D: "#forge:tools/screwdrivers",
-    R: "gtceu:wrought_iron_rod",
-    S: "gtceu:wrought_iron_screw",
-  })
+    // Loop material registry
+    GTRegistries.MATERIALS.forEach((material) => {
+        // Register rod recipes, if material has rod
+        if (material.hasFlag(GTMaterialFlags.GENERATE_ROD) && !m.hasProperty($PropertyKey.GEM)) {
+            event.custom({
+              type: "tfc:anvil",
+              input: {tag: "forge:ingots/" + material.name},
+              result: {
+                item: `gtceu:${material.name}_rod`,
+                count: 2,
+              },
+              tier: 3,
+              rules: ["bend_last", "draw_second_last", "draw_third_last"],
+            })
+            
+            // Register screw recipes, if material has screw
+            if (material.hasFlag(GTMaterialFlags.GENERATE_BOLT_SCREW)) {
+                event.custom({
+                    type: "tfc:welding",
+                    first_input: {item: `gtceu:${material.name}_bolt`},
+                    second_input: {item: `gtceu:${material.name}_bolt`},
+                    tier: 3,
+                    result: {item: `gtceu:${material.name}_screw`},
+                })
+                
+                if (material.hasProperty($PropertyKey.TOOL) && $Arrays.stream(material.getProperty($PropertyKey.TOOL).types).anyMatch(type => type == GTToolType.WIRE_CUTTER)) {
+                    event.custom({
+                        type: "tfc:welding",
+                        first_input: {item: `gtceu:${material.name}_rod`},
+                        second_input: {item: `gtceu:${material.name}_rod`},
+                        tier: 3,
+                        result: {item: `kubejs:${material.name}_cutter_head`},
+                      })
+                    event.shaped(`gtceu:${material.name}_wire_cutter`, [" F ", "HCD", "RSR"], {
+                      F: "#forge:tools/files",
+                      H: "#forge:tools/hammers",
+                      C: `kubejs:${material.name}_cutter_head`,
+                      D: "#forge:tools/screwdrivers",
+                      R: `gtceu:${material.name}_rod`,
+                      S: `gtceu:${material.name}_screw`,
+                    })
+                }
+            }
+        }
+    })
 
   event.custom({
     type: "tfc:glassworking",
