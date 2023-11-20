5d3d9ca6f55d9af1d18457405e28d463fe5af0bd
screret <68943070+screret@users.noreply.github.com>
2023-11-20 18:17:40 +0200

    add recipes for new machines

--- kubejs/server_scripts/constants_server.js
+++ kubejs/server_scripts/constants_server.js
@@ -6,6 +6,7 @@ const $Registries = Java.loadClass("net.minecraft.core.registries.Registries")
 const $FluidHelper = Java.loadClass("com.lowdragmc.lowdraglib.side.fluid.FluidHelper")
 const [ULV, LV, MV, HV, EV, IV, LuV, ZPM, UV, UHV, UEV, UIV, UXV, OpV, MAX] = GTValues.VA
 const RockBreakerCondition = Java.loadClass('com.gregtechceu.gtceu.common.recipe.RockBreakerCondition')
+const $CraftingComponent = Java.loadClass('com.gregtechceu.gtceu.data.recipe.CraftingComponent')
 
 const tfcSaplings = [
   "acacia",
