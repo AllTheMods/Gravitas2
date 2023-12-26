1f9b1769b7a8561a7977bce1c439b765a99eb865
screret <68943070+screret@users.noreply.github.com>
2023-12-26 21:31:53 +0200

    more circuits

--- kubejs/server_scripts/mods/gtceu/chains/circuits.js
+++ kubejs/server_scripts/mods/gtceu/chains/circuits.js
@@ -19,13 +19,85 @@ let gtceuCircuits = (/** @type {Internal.RecipesEventJS} */ event) => {
         .cleanroom(CleanroomType.STERILE_CLEANROOM)
         .EUt(UV).duration(200)
 
-    event.recipes.gtceu.circuit_assembler('bio_processor')
+    event.recipes.gtceu.circuit_assembler("bio_processor")
         .itemInputs([
             '1x gregitas:bioware_printed_circuit_board',
             '16x gregitas:bio_cells',
-            '8x gtceu:polybenzimidazole_pipe_small_fluid',
+            '8x gtceu:kapton_k_pipe_small_fluid',
             '8x gtceu:plutonium_241_plate',
-            '16x gtcue:styrene_butadiene_rubber_foil',
+            '16x gtceu:styrene_butadiene_rubber_foil',
             '8x gtceu:hastelloy_c_276_bolt'
         ])
+        .inputFluids('gtceu:bio_medium 250')
+        .itemOutputs('1x gregitas:bio_processor')
+        .cleanroom(CleanroomType.STERILE_CLEANROOM)
+        .EUt(160000).duration(200)
+        
+    event.recipes.gtceu.circuit_assembler("bioware_processor_uhv")
+        .itemInputs([
+            '1x gregitas:bio_processor',
+            '1x gtceu:qbit_cpu_chip',
+            '8x gregitas:highly_advanced_smd_capacitor',
+            '8x gregitas:highly_advanced_smd_transistor',
+            '8x gtceu:enriched_naquadah_trinium_europium_duranide_fine_wire'
+        ])
+        .inputFluids('gtceu:soldering_alloy 72')
+        .itemOutputs('2x gregitas:bioware_processor')
+        .cleanroom(CleanroomType.getByName("cleanerroom"))
+        .EUt(153600).duration(200)
+        
+    event.recipes.gtceu.circuit_assembler("bioware_processor_assembly_uev")
+        .itemInputs([
+            '1x gregitas:bioware_printed_circuit_board',
+            '2x gregitas:bioware_processor',
+            '6x gregitas:highly_advanced_smd_inductor',
+            '12x gregitas:highly_advanced_smd_capacitor',
+            '32x gtceu:ram_chip',
+            '16x gtceu:enriched_naquadah_trinium_europium_duranide_fine_wire'
+        ])
+        .inputFluids('gtceu:soldering_alloy 144')
+        .itemOutputs('2x gregitas:bioware_processor_assembly')
+        .cleanroom(CleanroomType.getByName("cleanerroom"))
+        .EUt(153600).duration(400)
+        
+    event.recipes.gtceu.assembly_line("bioware_super_computer_uiv")
+        .itemInputs([
+            '1x gregitas:bioware_printed_circuit_board',
+            '2x gregitas:bioware_processor_computer',
+            '8x gregitas:highly_advanced_smd_diode',
+            '32x gtceu:nor_memory_chip',
+            '64x gtceu:ram_chip',
+            '48x gtceu:enriched_naquadah_trinium_europium_duranide_fine_wire',
+            '4x gtceu:neutronium_plate',
+        ])
+        .inputFluids('gtceu:soldering_alloy 1152')
+        .itemOutputs('1x gregitas:bioware_processor_computer')
+        .cleanroom(CleanroomType.getByName("cleanerroom"))
+        .EUt(153600).duration(400)
+        
+    event.recipes.gtceu.assembly_line("bioware_mainframe_uxv")
+        .itemInputs([
+            '2x gtceu:neutronium_frame', 
+            '2x gregitas:bioware_processor_computer',
+            '32x gregitas:highly_advanced_smd_diode',
+            '32x gregitas:highly_advanced_smd_capacitor',
+            '32x gregitas:highly_advanced_smd_transistor',
+            '32x gregitas:highly_advanced_smd_resistor',
+            '32x gregitas:highly_advanced_smd_inductor',
+            '64x gtceu:kapton_k_foil',
+            '64x gtceu:ram_chip',
+            '16x gtceu:ruthenium_trinium_americium_neutronate_double_wire',
+            '8x gtceu:neutronium_plate',
+        ])
+        .inputFluids([
+            'gtceu:soldering_alloy 2880',
+            'gtceu:kapton_k 1152',
+        ])
+        .itemOutputs('1x gregitas:bioware_processor_mainframe')
+        .cleanroom(CleanroomType.getByName("cleanerroom"))
+        .EUt(1200000).duration(2000)
+
+  //sculkware
+    event.recipes.gtceu.brewery("cultivate_sculk")
+        .
 }
