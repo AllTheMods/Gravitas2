05fc2b3a849808a4ac5a3be4866fb9e5011b4187
Angel-Abaddon <155321820+Angel-Abaddon@users.noreply.github.com>
2023-12-31 16:41:44 -0500

    Update createAdd.js

--- kubejs/server_scripts/mods/create/createAdd.js
+++ kubejs/server_scripts/mods/create/createAdd.js
@@ -4,6 +4,409 @@ let createAdd = (/** @type {Internal.RecipesEventJS} */ event) => {
 
     event.custom(
         {
+            type: 'create:mixing',
+            ingredients: [
+                {
+                    fluid: 'tfc:olive_oil',
+                    nbt: {},
+                    amount: 1000
+                },
+                {
+                    tag: 'tfc:foods/dough'
+                },
+                {
+                    item: 'tfc:powder/salt'
+                },
+                {
+                    item: 'firmalife:spice/basil_leaves'
+                },
+            ],
+            results: [
+                {
+                    item: 'firmalife:food/pizza_dough',
+                    count: 4
+                }
+            ],
+        }
+    )
+    event.custom(
+        {
+            type: 'create:mixing',
+            ingredients: [
+                {
+                    fluid: 'firmalife:cream',
+                    nbt: {},
+                    amount: 1000
+                },
+                {
+                    tag: 'firmalife:sweetener'
+                },
+                {
+                    item: 'firmalife:ice_shavings'
+                },
+                {
+                    item: 'firmalife:spice/vanilla'
+                },
+            ],
+            results: [
+                {
+                    item: 'firmalife:food/vanilla_ice_cream',
+                    count: 2
+                }
+            ],
+        }
+    )
+    event.custom(
+        {
+            type: 'create:mixing',
+            ingredients: [
+                {
+                    fluid: 'minecraft:water',
+                    nbt: {},
+                    amount: 1000
+                },
+                {
+                    tag: 'firmalife:sweetener'
+                },
+                {
+                    tag: 'firmalife:feeds_yeast'
+                },
+                {
+                    item: 'minecraft:egg'
+                },
+                {
+                    item: 'tfc:food/pumpkin_chunks'
+                },
+                {
+                    item: 'tfc:food/pumpkin_chunks'
+                },
+            ],
+            results: [
+                {
+                    item: 'firmalife:food/pumpkin_pie_dough',
+                    count: 1
+                }
+            ],
+        }
+    )
+    event.custom(
+        {
+            type: 'create:mixing',
+            ingredients: [
+                {
+                    fluid: 'firmalife:cream',
+                    nbt: {},
+                    amount: 1000
+                },
+                {
+                    item: 'tfc:powder/salt'
+                },
+            ],
+            results: [
+                {
+                    item: 'firmalife:food/butter',
+                    count: 1
+                }
+            ],
+        }
+    )
+    event.custom(
+        {
+            type: 'create:mixing',
+            ingredients: [
+                {
+                    fluid: 'minecraft:water',
+                    nbt: {},
+                    amount: 1000
+                },
+                {
+                    tag: 'firmalife:sweetener'
+                },
+                {
+                    tag: 'firmalife:feeds_yeast'
+                },
+                {
+                    item: 'firmalife:food/butter'
+                },
+            ],
+            results: [
+                {
+                    item: 'firmalife:food/pie_dough',
+                    count: 1
+                }
+            ],
+        }
+    )
+    event.custom(
+        {
+            type: 'create:mixing',
+            ingredients: [
+                {
+                    item: 'minecraft:egg'
+                },
+                {
+                    item: 'firmalife:spice/vanilla'
+                },
+                {
+                    item: 'firmalife:food/butter'
+                },
+                {
+                    tag: 'firmalife:sweetener'
+                },
+                {
+                    tag: 'firmalife:feeds_yeast'
+                },
+            ],
+            results: [
+                {
+                    item: 'firmalife:food/cookie_dough',
+                    count: 4
+                }
+            ],
+        }
+    )
+    event.custom(
+        {
+            type: 'create:mixing',
+            ingredients: [
+                {
+                    fluid: 'minecraft:water',
+                    nbt: {},
+                    amount: 1000
+                },
+                {
+                    tag: 'firmalife:feeds_yeast'
+                },
+                {
+                    item: 'tfc:powder/salt'
+                },
+            ],
+            results: [
+                {
+                    item: 'firmalife:food/hardtack_dough',
+                    count: 4
+                }
+            ],
+        }
+    )
+    event.custom(
+        {
+            type: 'create:mixing',
+            ingredients: [
+                {
+                    fluid: 'firmalife:chocolate',
+                    nbt: {},
+                    amount: 1000
+                },
+                {
+                    item: 'firmalife:food/vanilla_ice_cream'
+                },
+            ],
+            results: [
+                {
+                    item: 'firmalife:food/chocolate_ice_cream',
+                    count: 1
+                }
+            ],
+        }
+    )
+    event.custom(
+        {
+            type: 'create:mixing',
+            ingredients: [
+                {
+                    fluidTag: 'tfc:milks',
+                    nbt: {},
+                    amount: 1000
+                },
+                {
+                    tag: 'firmalife:sweetener'
+                },
+                {
+                    item: 'firmalife:food/cocoa_butter'
+                },
+                {
+                    item: 'firmalife:food/cocoa_butter'
+                },
+            ],
+            results: [
+                {
+                    item: 'firmalife:food/white_chocolate_blend',
+                    count: 2
+                }
+            ],
+        }
+    )
+    event.custom(
+        {
+            type: 'create:mixing',
+            ingredients: [
+                {
+                    fluidTag: 'tfc:milks',
+                    nbt: {},
+                    amount: 1000
+                },
+                {
+                    tag: 'firmalife:sweetener'
+                },
+                {
+                    item: 'firmalife:food/cocoa_powder'
+                },
+                {
+                    item: 'firmalife:food/cocoa_powder'
+                },
+            ],
+            results: [
+                {
+                    item: 'firmalife:food/dark_chocolate_blend',
+                    count: 2
+                }
+            ],
+        }
+    )
+    event.custom(
+        {
+            type: 'create:mixing',
+            ingredients: [
+                {
+                    tag: 'firmalife:foods/chocolate'
+                },
+                {
+                    item: 'firmalife:food/cookie_dough'
+                },
+                {
+                    item: 'firmalife:food/cookie_dough'
+                },
+                {
+                    item: 'firmalife:food/cookie_dough'
+                },
+                {
+                    item: 'firmalife:food/cookie_dough'
+                },
+            ],
+            results: [
+                {
+                    item: 'firmalife:food/chocolate_chip_cookie_dough',
+                    count: 4
+                }
+            ],
+        }
+    )
+    event.custom(
+        {
+            type: 'create:mixing',
+            ingredients: [
+                {
+                    fluidTag: 'tfc:milks',
+                    nbt: {},
+                    amount: 1000
+                },
+                {
+                    tag: 'firmalife:sweetener'
+                },
+                {
+                    item: 'firmalife:food/cocoa_butter'
+                },
+                {
+                    item: 'firmalife:food/cocoa_powder'
+                },
+            ],
+            results: [
+                {
+                    item: 'firmalife:food/milk_chocolate_blend',
+                    count: 2
+                }
+            ],
+        }
+    )
+    event.custom(
+        {
+            type: 'create:mixing',
+            ingredients: [
+                {
+                    item: 'firmalife:food/vanilla_ice_cream'
+                },
+                {
+                    item: 'tfc:food/strawberry'
+                },
+                {
+                    item: 'tfc:food/strawberry'
+                },
+            ],
+            results: [
+                {
+                    item: 'firmalife:food/strawberry_ice_cream',
+                    count: 1
+                }
+            ],
+        }
+    )
+    ///fixing firmalife cocoa powder recipe
+    event.remove({ id: "firmalife:crafting/cocoa_butter_powder" })
+    event.custom({
+        type: "tfc:damage_inputs_shapeless_crafting",
+        recipe: {
+            type: "minecraft:crafting_shapeless",
+            ingredients: [
+            {
+                item: "firmalife:food/roasted_cocoa_beans"
+            },
+            {
+                tag: "forge:tools/hammers"
+            }
+            ],
+            result: {
+                item: "firmalife:food/cocoa_powder"
+            }
+        }
+      })
+
+    event.custom({
+        type: "tfc:damage_inputs_shapeless_crafting",
+        recipe: {
+            type: "minecraft:crafting_shapeless",
+            ingredients: [
+            {
+                item: "firmalife:food/roasted_cocoa_beans"
+            },
+            {
+                tag: "forge:tools/knives"
+            }
+            ],
+            result: {
+                item: "firmalife:food/cocoa_butter"
+            }
+        }
+      })
+
+    event.custom(
+        {
+            type: 'create:mixing',
+            ingredients: [
+                {
+                tag: 'firmalife:sweetener'
+            },
+            {
+                tag: 'firmalife:foods/chocolate'
+            },
+            {
+                fluidTag: 'tfc:milks',
+                nbt: {},
+                amount: 1000
+            }
+        ],
+        results: [
+            {
+                fluid: 'firmalife:chocolate',
+                nbt: {},
+                amount: 1000
+            }
+        ],
+        heatRequirement: "heated"
+        }
+    )
+    event.custom(
+        {
             type: 'tfc:casting',
             mold: {
                 item: 'tfc:ceramic/ingot_mold'
