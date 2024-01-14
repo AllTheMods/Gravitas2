77364526940b7a1ffa5e38dad666cd2e64f4bc24
Drackion <64641342+Drackion@users.noreply.github.com>
2024-01-14 18:55:37 +1100

    forgor tags

--- kubejs/server_scripts/tags/item_tags.js
+++ kubejs/server_scripts/tags/item_tags.js
@@ -256,7 +256,7 @@ const addItemTags = (/** @type {TagEvent.Item} */ event) => {
   event.remove("forge:dusts/copper", ["tfc:powder/malachite", "tfc:powder/tetrahedrite", "tfc:powder/native_copper"])
   event.add("forge:plates/aluminum", "#forge:plates/aluminium")
   event.add("forge:nuggets/aluminum", "#forge:nuggets/aluminium")
-  event.add("tfc:forge_fuel", ["gtceu:coke_gem", "gtceu:coke_block","gtceu:coke_chipped_gem","gtceu:coke_flawed_gem"])
+  event.add("tfc:forge_fuel", ["gtceu:coke_gem", "gtceu:coke_block","gtceu:coke_chipped_gem","gtceu:coke_flawed_gem", 'gtceu:coke_flawless_gem', 'gtceu:coke_exquisite_gem'])
   event.add("forge:dusts/sulfur", "tfc:powder/sulfur")
   event.remove("forge:ingots/iron", "tfc:metal/ingot/wrought_iron")
 
