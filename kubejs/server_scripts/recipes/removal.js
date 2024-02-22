a3e237a4700c52dabe14ddc6e360d8ec3442a7c4
thevortex <james.g.bowley@gmail.com>
2024-02-22 08:38:45 -0700

    remove eater of worlds

--- kubejs/server_scripts/recipes/removal.js
+++ kubejs/server_scripts/recipes/removal.js
@@ -126,6 +126,7 @@ let recipeRemoval = (/** @type {Internal.RecipesEventJS} */ event) => {
   event.remove({ id: /^create:crushing\/.*_block/, mod: "create" })
   event.remove({ id: /^create:crushing\/.*_armor/, mod: "create" })
   event.remove({ id: "create:crafting/appliances/copper_backtank"})
+  event.remove({ id: "create:crafting/appliances/tree_fertilizer"})
   event.remove({ id: "create:crushing/blaze_rod"})
   event.remove({ id: "create:crafting/kinetics/fluid_tank"})
   event.remove({ id: "create:crafting/kinetics/mechanical_press"})
