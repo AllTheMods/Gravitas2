34b1f0c4283452e0031036fbea353e0997968bfd
Azaysol <abdyman@gmail.com>
2024-05-28 21:49:28 -0400

    Update gregitas.js

--- kubejs/client_scripts/lang/gregitas.js
+++ kubejs/client_scripts/lang/gregitas.js
@@ -2,4 +2,6 @@
 
 const addGregitasName = (event) => {
   event.add("jade.modName.gregitas", "Gravitas²")
+  event.add("fluid_type.thoriumreactors.steam", "Superheated Steam")
+  event.add("item.thoriumreactors.steam_bucket", "Superheated Steam Bucket")
 }
