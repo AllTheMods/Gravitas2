eccc05f2e072ea8b0ae6907a438dd6deb8cc6c02
John Rowley <johnrowleyster@gmail.com>
2024-11-30 16:22:31 -0800

    chore: run prettier on kubejs startup_scripts

--- kubejs/startup_scripts/registry/fluids.js
+++ kubejs/startup_scripts/registry/fluids.js
@@ -1,31 +1,11 @@
 // priority 10
 
 let registerFluids = (/** @type {Registry.Fluid} */ event) => {
-    event.create('gregitas:raw_syrup')
-        .thinTexture(0xBB9351)
-        .displayName('Raw Maple Sap')
-
-    event.create('gregitas:maple_syrup')
-        .thickTexture(0xBB9351)
-        .displayName('Maple Syrup')
-
-    event.create("gregitas:cane_syrup")
-        .thickTexture(0xB67050)
-        .displayName("Cane Syrup")
-
-    event.create("gregitas:sugar_syrup")
-        .thickTexture(0xFFF9DB)
-        .displayName("Sugar Syrup")
-
-    event.create("gregitas:raw_resin")
-        .thickTexture(0x785808)
-        .displayName("Raw Resin")
-
-    event.create("gregitas:crimson_resin")
-        .thickTexture(0xB62A0C)
-        .displayName("Crimson Resin")
-
-    event.create("gregitas:warped_resin")
-        .thickTexture(0x000F75)
-        .displayName("Warped Resin")
+  event.create("gregitas:raw_syrup").thinTexture(0xbb9351).displayName("Raw Maple Sap")
+  event.create("gregitas:maple_syrup").thickTexture(0xbb9351).displayName("Maple Syrup")
+  event.create("gregitas:cane_syrup").thickTexture(0xb67050).displayName("Cane Syrup")
+  event.create("gregitas:sugar_syrup").thickTexture(0xfff9db).displayName("Sugar Syrup")
+  event.create("gregitas:raw_resin").thickTexture(0x785808).displayName("Raw Resin")
+  event.create("gregitas:crimson_resin").thickTexture(0xb62a0c).displayName("Crimson Resin")
+  event.create("gregitas:warped_resin").thickTexture(0x000f75).displayName("Warped Resin")
 }
