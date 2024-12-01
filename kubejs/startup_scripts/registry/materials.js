7495af35639e73eb44b4a7c4070efb29e8296d9f
John Rowley <johnrowleyster@gmail.com>
2024-11-30 16:22:31 -0800

    chore: run prettier on startup_scripts and gtceu scripts

--- kubejs/startup_scripts/registry/materials.js
+++ kubejs/startup_scripts/registry/materials.js
@@ -1,15 +1,12 @@
 // priority 10
 
 let registerGTCEuMaterialFlags = (/** @type {Registry.Material} */ event) => {
-    GTMaterials.get("bismuth").addFlags(GTMaterialFlags.GENERATE_PLATE, GTMaterialFlags.GENERATE_ROD)
-    GTMaterials.get("nickel").addFlags(GTMaterialFlags.GENERATE_PLATE, GTMaterialFlags.GENERATE_ROD)
-    GTMaterials.get("astatine").setProperty(PropertyKey.DUST, new $DustProperty())
-    GTMaterials.get("iodine").setProperty(PropertyKey.DUST, new $DustProperty())
+  GTMaterials.get("bismuth").addFlags(GTMaterialFlags.GENERATE_PLATE, GTMaterialFlags.GENERATE_ROD)
+  GTMaterials.get("nickel").addFlags(GTMaterialFlags.GENERATE_PLATE, GTMaterialFlags.GENERATE_ROD)
+  GTMaterials.get("astatine").setProperty(PropertyKey.DUST, new $DustProperty())
+  GTMaterials.get("iodine").setProperty(PropertyKey.DUST, new $DustProperty())
 
-    GTMaterials.get("certus_quartz").setFormula('CtO2', true)
+  GTMaterials.get("certus_quartz").setFormula("CtO2", true)
 
-    event.create("polycertus")
-      .gem()
-      .color(0xb6f2ee)
-      .components('1x certus')
+  event.create("polycertus").gem().color(0xb6f2ee).components("1x certus")
 }
