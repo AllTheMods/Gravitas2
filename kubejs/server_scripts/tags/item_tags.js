037b0bcf0a2382bb7ff3b817e01568a9ebb4d663
Justin Charron <xjsc16x@gmail.com>
2024-12-28 17:22:18 -0500

    add tfc magma to IE thermoelectric generator

--- kubejs/server_scripts/tags/item_tags.js
+++ kubejs/server_scripts/tags/item_tags.js
@@ -412,4 +412,14 @@ const addItemTags = (/** @type {TagEvent.Item} */ event) => {
 
   event.add("gravitas:phantom", "minecraft:warped_fungus")
   event.add("gravitas:phantom", "beneath:wood/sapling/warped")
+
+  event.add("forge:magma", ["minecraft:magma_block",
+                            "tfc:rock/magma/basalt",
+                            "tfc:rock/magma/granite",
+                            "tfc:rock/magma/diorite",
+                            "tfc:rock/magma/gabbro",
+                            "tfc:rock/magma/rhyolite",
+                            "tfc:rock/magma/andesite",
+                            "tfc:rock/magma/dacite"
+                           ])
 }
