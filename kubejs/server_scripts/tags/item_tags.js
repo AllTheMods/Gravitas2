8aa5327bf7b266ee6900c5b0137fbc104dd80b4b
ACCount <ACCount12@users.noreply.github.com>
2025-05-04 18:44:08 +0300

    Adds a few tools to TFC tool racks

--- kubejs/server_scripts/tags/item_tags.js
+++ kubejs/server_scripts/tags/item_tags.js
@@ -197,7 +197,7 @@ const addItemTags = (/** @type {TagEvent.Item} */ event) => {
   event.add("forge:tools/hammers", "#tfc:hammers")
   event.add("forge:tools/knives", ["#tfc:knives"])
 
-  event.add("forge:tools", ["#forge:tools/saws", "#forge:tools/hammers", "#forge:tools/knives", "#forge:tools/files"])
+  event.add("forge:tools", ["#forge:tools/saws", "#forge:tools/hammers", "#forge:tools/wrench", "#forge:tools/knives", "#forge:tools/files"])
 
   gtceuIngots.forEach((ingot) => {
     event.add("tfc:pileable_ingots", `gtceu:${ingot}_ingot`)
@@ -335,7 +335,9 @@ const addItemTags = (/** @type {TagEvent.Item} */ event) => {
     "mininggadgets:mininggadget",
     "mininggadgets:mininggadget_fancy",
     "mininggadgets:mininggadget_simple",
-    "create:wrench",
+	"integrateddynamics:labeller",
+	"integrateddynamics:portable_logic_programmer",
+	"tfc:wool_cloth",
     "buildinggadgets2:gadget_building",
     "buildinggadgets2:gadget_exchanging",
     "buildinggadgets2:gadget_copy_paste",
